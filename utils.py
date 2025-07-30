5234f4f48656e4b74e4911cbb9eca78a819b94c4
Octavio Pechin <69601587+o8vio@users.noreply.github.com>
2025-07-30 14:31:48 -0300

    Add files via upload

--- utils.py
+++ utils.py
@@ -92,15 +92,14 @@ def plot_results(param_name, param_range, results, **kwargs):
 
   n_features = results.shape[2]
 
-  n_rows = n_features // 2 + 1 if n_features % 2 == 1 else n_features // 2
+  n_rows, n_columns = kwargs.get('n_rows', int(np.ceil(n_features / 2))), kwargs.get('n_columns', 2)
 
-  fig, axs = plt.subplots(n_rows, 2, figsize=(12, 4 * n_rows))
+  fig, axs = plt.subplots(n_rows, n_columns, constrained_layout=True)
   axs = np.atleast_1d(axs).flatten()
 
-  is_mix_counter_ = count(1)
   titles = kwargs['titles']
 
-  if param_name == 'world dim':
+  if param_name == 'world_dim':
 
     dim_values = param_range.copy()
     param_range_ = np.arange(len(dim_values))
@@ -112,44 +111,48 @@ def plot_results(param_name, param_range, results, **kwargs):
   for i in range(n_features):
 
     ax = axs[i]
-    
+
     ax.set_title(titles[i])
-    ax.set_aspect('equal')
+    
+    ax.set_ylim(bottom=0, top=1)
 
     t_means = results[:,:,i,0].mean(axis=1)
     t_stds = results[:,:,i,0].std(axis=1)
     v_means = results[:,:,i,1].mean(axis=1)
     v_stds = results[:,:,i,1].std(axis=1)
 
-    ax.plot(param_range, t_means, '-o', color='tab:blue', label='training score')
-    ax.plot(param_range, v_means, '-o', color='tab:green', label='validation score')
+    ax.plot(param_range_, t_means, '-o', color='tab:blue', label='training score')
+    ax.plot(param_range_, v_means, '-o', color='tab:green', label='validation score')
 
     if kwargs.get('fill', True):
 
-      ax.fill_between(param_range, t_means - t_stds, t_means + t_stds, alpha=0.1, color='tab:blue')
-      ax.fill_between(param_range, v_means - v_stds, v_means + v_stds, alpha=0.1, color='tab:green')
+      ax.fill_between(param_range_, t_means - t_stds, t_means + t_stds, alpha=0.1, color='tab:blue')
+      ax.fill_between(param_range_, v_means - v_stds, v_means + v_stds, alpha=0.1, color='tab:green')
 
+    ax.set_xticks(param_range_)
+    
     if param_name == 'world_dim':
 
-      ax.set_xticks(param_range)
       ax.set_xticklabels(dim_values)
 
     ax.legend(loc='lower right', fontsize=8)
     ax.set_xlabel(param_name)
     ax.set_ylabel(kwargs.get('y_label', 'accuracy'))
-
+  
   for j in range(n_features, len(axs)):
      
      fig.delaxes(axs[j])
   
-  plt.tight_layout()
-  
   if kwargs.get('save', True):
+     
      fig.savefig(kwargs.get('figname', './figure.pdf'))
   
   if kwargs.get('show', True):
+     
      plt.show()
+
   else:
+     
      plt.close(fig)
 
 
